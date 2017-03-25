91b4a32be0aed4e9d92807e2cc980fd978f1c9ea
Carson Dobiash <Carson@Carsons-Macbook.local>
2017-03-25 18:23:31 -0500

    atbash encryption added

---
+++ atbash.js
@@ -0,0 +1,17 @@
+function atbash(statement){
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var reverse = "zyxwvutsrqponmlkjihgfedcba";
+	var decoded = '';
+
+	for(var i = 0; i < statement.length; ++i){
+		var letter = statement.charAt(i);
+		var index = alphabet.indexOf(letter);
+		var encryptedLetter = reverse.charAt(index);
+
+		decoded += encryptedLetter;
+	}
+	return decoded;
+}
+
+console.log(atbash("abcdefghijklmnopqrstuvwxyz"));
+
