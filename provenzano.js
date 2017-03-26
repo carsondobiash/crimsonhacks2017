6755c4ef46cf63e4528255213ad584baf9e66b2c
Raychel <rhdelaney@crimson.ua.edu>
2017-03-26 01:07:38 -0500

    life alert help I've coded and I can't get up

--- provenzano.js
+++ provenzano.js
@@ -1,14 +1,18 @@
       document.getElementById("encryptMe").onclick = function (){
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
         var decoded = '';
         var valueInput = document.getElementById("input").value;
-
-        for(var i = valueInput.length - 1; i >= 0; i--){
+        var kmove=document.getElementById("key").value;
+        for(var i = 0; i < valueInput.length; ++i){
           var letter = valueInput.charAt(i);
-          
           if(letter == " "){
             var encryptedLetter = " ";
           }else{
-            var encryptedLetter = letter;            
+            for (var j = 0;j<alphabet.length; j++) {
+              if(alphabet.charAt(j)==letter.isLower()){
+                var encryptedLetter = j + kmove;
+              }
+            }           
           }
           decoded += encryptedLetter;
         }
