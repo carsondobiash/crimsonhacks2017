c7f5bd0d6ac0110f69b12d195bf9e67b48271838
Raychel <rhdelaney@crimson.ua.edu>
2017-03-26 02:34:21 -0500

    need help fixing provenzano

--- provenzano.js
+++ provenzano.js
@@ -1,18 +1,34 @@
-      document.getElementById("encryptMe").onclick = function (){
+function encrypt(){
         var alphabet = "abcdefghijklmnopqrstuvwxyz";
         var decoded = '';
         var valueInput = document.getElementById("input").value;
         var kmove=document.getElementById("key").value;
         for(var i = 0; i < valueInput.length; ++i){
-          var letter = valueInput.charAt(i);
+          var letter = valueInput.charAt(i).toLowerCase();
           if(letter == " "){
             var encryptedLetter = " ";
           }else{
             for (var j = 0;j<alphabet.length; j++) {
-              if(alphabet.charAt(j)==letter.isLower()){
-                var encryptedLetter = j + kmove;
+              if(alphabet.charAt(j)==letter){
+                var encryptedLetter = ((j + parseInt(kmove))%26).toString() + " ";
               }
             }           
+          }
+          decoded += encryptedLetter;
+        }
+        document.getElementById("output").value = decoded;
+      } 
+function decrypt (){
+       var alphabet = "abcdefghijklmnopqrstuvwxyz";
+        var decoded = '';
+        var valueInput = document.getElementById("input").value;
+        var kmove=document.getElementById("key").value;
+        for(var i = 0; i < valueInput.length; ++i){
+          var num = parseInt(valueInput.charAt(i));
+          if(num == " "){
+            var encryptedLetter = " ";
+          }else{
+            var encryptedLetter = (alphabet.charAt(num+j)).toString() + " ";           
           }
           decoded += encryptedLetter;
         }
